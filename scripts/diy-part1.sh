da3f8926316f1159380b961dbd35dfbce48e9459
zhuixing05 <36839600+zhuixing05@users.noreply.github.com>
2026-06-24 12:05:23 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# =========================================
 # DIY Part 1 - Before Update feeds
+# =========================================
 
-echo "执行 diy-part1.sh"
+# 示例：如果以后需要添加第三方 feed，可以取消下面注释
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git luci-app-ssr-plus https://github.com/fw876/helloworld' feeds.conf.default
