2df4fac4f38fe61fa7acb477ced1fcab30d78ec9
zhuixing05 <36839600+zhuixing05@users.noreply.github.com>
2026-06-24 11:50:02 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # DIY Part 2 - After Update feeds（ImmortalWrt Master）
 
+# ==================== 基础设置 ====================
 # 设置 root 密码为 kantanvpn
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
@@ -8,15 +9,62 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='immortalwrt'" >> package/base-files/files/etc/openwrt_release
 
-# 添加 Argon 主题
+# ==================== LAN IP 修改 ====================
+# 修改 LAN IP 为 192.168.133.1
+sed -i "s/192.168.1.1/192.168.133.1/g" package/base-files/files/bin/config_generate
+
+# ==================== 添加主题 ====================
 rm -rf package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 rm -rf package/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
-# 添加 frpc（如需）
+# ==================== 添加最新版 frpc ====================
+# 删除旧版 frpc
+rm -rf package/frp
 rm -rf package/luci-app-frpc
+
+# 添加最新版 frpc 和 luci-app-frpc
 git clone https://github.com/breakertian/luci-app-frpc.git package/luci-app-frpc
+git clone https://github.com/fatedier/frp.git package/frp
+
+# ==================== 首次启动自动配置脚本 ====================
+mkdir -p package/base-files/files/etc/uci-defaults
+
+cat > package/base-files/files/etc/uci-defaults/99-custom-setup << 'EOF'
+#!/bin/sh
+exec >/tmp/setup.log 2>&1
+
+# LAN IP
+uci set network.lan.ipaddr='192.168.133.1'
+uci commit network
+
+# frpc 配置
+uci -q delete frpc.main
+uci set frpc.main=server
+uci set frpc.main.server_addr='frp.zhixiang.us'
+uci set frpc.main.server_port='7000'
+uci set frpc.main.token='asdfghjkl'
+uci set frpc.main.log_file='/var/log/frpc.log'
+uci set frpc.main.log_level='info'
+
+uci -q delete frpc.kantan_web
+uci set frpc.kantan_web=proxy
+uci set frpc.kantan_web.name='kantan_web'
+uci set frpc.kantan_web.type='http'
+uci set frpc.kantan_web.local_ip='127.0.0.1'
+uci set frpc.kantan_web.local_port='880'
+uci set frpc.kantan_web.custom_domains='kantan.zhixiang.us'
+uci set frpc.kantan_web.use_encryption='1'
+uci set frpc.kantan_web.use_compression='1'
+
+uci commit frpc
+/etc/init.d/frpc enable
+
+echo "All done!"
+EOF
+
+chmod +x package/base-files/files/etc/uci-defaults/99-custom-setup
 
 echo "diy-part2.sh 执行完成"
